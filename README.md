1d827395cedfe5a235b505fe023df774800ece60
alphaomega325 <alphaomega325@gmail.com>
2018-10-12 17:34:41 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a basic xor generator for basic machine learning applications.  It is only able to generate csv files.
---
+++ src/csvgen.java
@@ -0,0 +1,60 @@
+/*
+  @author:alphaomega325
+  @date:10/12/18
+  Purpose is to create a simple xor generator for machine learning testing
+
+*/
+
+import java.io.Writer;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class csvgen {
+
+
+    public static void main(String [] args)
+    {
+
+	Scanner in = new Scanner(System.in);
+	
+	System.out.println("How many lines do you want for the csv file");
+
+	String input = in.next();
+
+	float length = Float.parseFloat(input);
+	
+	try {
+	    PrintWriter writer = new PrintWriter("xorgen.csv", "UTF-8");
+
+		writer.print("first,second,result\n");
+		Random rand = new Random();
+
+	    
+	    for(float i = 0; i < length; i++)
+		{
+		    int x1 = rand.nextInt(2);
+		    int x2 = rand.nextInt(2);
+		    int y = x1^x2;
+		    
+		    writer.print(x1+","+x2+","+y+"\n");
+		    
+		}
+	    
+	    writer.close();
+	    
+	} catch (IOException x) {
+
+	    System.err.println(x);
+	    
+	}
+
+
+    }
+
+
+
+
+
+}
